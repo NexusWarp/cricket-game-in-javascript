28cdcb0de9690ce03f3e1f7183a126eccf30a3f1
NexusWarp <ik571572@gmail.com>
2023-12-24 10:28:37 +0500

    FUNCATIONALTY

--- game.js
+++ game.js
@@ -2,7 +2,8 @@ let score = 0;
 let wickets = 0;
 let overs = 0;
 let balls = 0;
-
+let oversSelected = 1;
+let target;
 let playBtn = document.querySelector(".play");
 let fiveBtn = document.querySelector(".five");
 let tenBtn = document.querySelector(".ten");
@@ -15,10 +16,12 @@ fiveBtn.style.backgroundColor = "rgb(238, 146, 54)" ;
 fiveBtn.addEventListener("click" , ()=>{
     fiveBtn.style.backgroundColor = "rgb(238, 146, 54)" ;
     tenBtn.style.backgroundColor = " rgb(71, 70, 69)" ;
+    oversSelected = 5;
 })
 tenBtn.addEventListener("click", ()=>{
     fiveBtn.style.backgroundColor = "rgb(71, 70, 69)" ;
-    tenBtn.style.backgroundColor = " rgb(238, 146, 54)" ;
+    tenBtn.style.backgroundColor = " rgb(238, 146, 54)";
+    oversSelected = 10;
 })
 
 
@@ -33,19 +36,19 @@ playBtn.addEventListener('click', ()=>{
     document.querySelector(".ball").classList.remove("ballanimate");
     document.querySelector(".bat").classList.remove("batanimate");
     if(random==0){
-        balls ++;
+       
         document.querySelector(".balls").innerHTML = balls;
         shotAnim(0);
     }
     else if(random ==1){
-        balls ++;
+    
         score++;
         document.querySelector(".score").innerHTML = score;
         document.querySelector(".balls").innerHTML = balls;
         shotAnim(1);
     }
     else if(random ==2){
-        balls ++;
+        
         score +=2;
 
         shotAnim(2);
@@ -53,28 +56,28 @@ playBtn.addEventListener('click', ()=>{
         document.querySelector(".balls").innerHTML = balls;
     }
     else if(random ==3){
-        balls ++;
+        
         score +=3;
         document.querySelector(".score").innerHTML = score;
         document.querySelector(".balls").innerHTML = balls;
         shotAnim(3);
     }
     else if(random ==4){
-        balls ++;
+        
         score +=4;
         document.querySelector(".score").innerHTML = score;
         document.querySelector(".balls").innerHTML = balls;
         shotAnim(4);
     }
     else if(random ==5){
-        balls ++;
+        
         wickets++;
         document.querySelector(".wickets").innerHTML = wickets;
         document.querySelector(".balls").innerHTML = balls;
         shotAnim("W");
     }
     else if(random ==6){
-        balls ++;
+       
         score +=6;
         document.querySelector(".score").innerHTML = score;
         document.querySelector(".balls").innerHTML = balls;
@@ -85,8 +88,34 @@ playBtn.addEventListener('click', ()=>{
 })
 function shotAnim(per){
     document.querySelector(".shot").innerHTML = per;
+    
     document.querySelector(".shot").classList.add("shotanimate");
     setTimeout(()=>{
         document.querySelector(".shot").classList.remove("shotanimate");
+        overCount();
     },1000)
+}
+function overCount(){
+    balls ++;
+    document.querySelector(".balls").innerHTML = balls;
+    if(balls>5){
+        balls = 0;
+        overs++;
+        document.querySelector(".overs").innerHTML = overs;
+        
+    }
+    if(overs==oversSelected){
+        document.querySelector(".teamname").innerHTML="Team 2 Batting";
+        target = score+1;
+        document.querySelector(".target").innerHTML ="Target: " + target;
+        score = 0;
+        wickets=0;
+        overs=0;
+        balls=0;
+        document.querySelector(".balls").innerHTML = balls;
+        document.querySelector(".score").innerHTML = score;
+        document.querySelector(".wickets").innerHTML = wickets;
+        document.querySelector(".overs").innerHTML = overs;
+    }
+   
 }
